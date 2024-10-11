0a0c8bb9873d4532bf082973a71335d49fb323b8
Dow Drake <dowdrake@gmail.com>
2024-10-11 14:33:12 -0700

    Override login and logout views to manage django_language cookie

--- account/views.py
+++ account/views.py
@@ -1,13 +1,37 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, TemplateView, RedirectView
 from django.views import View
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy, reverse
-from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str  # force_text on older versions of Django
+from django.utils.http import urlsafe_base64_decode
 
 from .forms import RegistrationForm, token_generator, user_model
 
+# Override the contrib auth LoginView to manage the session variable
+class MyLoginView(LoginView):
+    """Security check complete. Log the user in."""
+    def form_valid(self, form):
+        lang = self.request.COOKIES.get('django_language', 'en-us')
+        auth_login(self.request, form.get_user())
+        response = HttpResponseRedirect(self.get_success_url())
+        response.set_cookie('django_language', lang)
+        return response
+
+class MyLogoutView(LogoutView):
+    def post(self, request, *args, **kwargs):
+        """Logout may be done via POST."""
+        lang = self.request.COOKIES.get('django_language', 'en-us')
+        auth_logout(request)
+        redirect_to = self.get_success_url()
+        if redirect_to != request.get_full_path():
+            # Redirect to target page once the session has been cleared.
+            response = HttpResponseRedirect(redirect_to)
+            response.set_cookie('django_language', lang)
+            return response
+        return super().get(request, *args, **kwargs)
 
 class RegistrationView(CreateView):
     form_class = RegistrationForm
@@ -46,7 +70,7 @@ class ActivateView(RedirectView):
         if user is not None and token_generator.check_token(user, token):
             user.is_active = True
             user.save()
-            login(request, user)
+            auth_login(request, user)
             return super().get(request, uidb64, token)
         else:
             return render(request, 'account/activate_account_invalid.html')

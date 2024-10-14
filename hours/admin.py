39bde88ca520ec7f0102855484bda4043e1614cf
Dow Drake <dowdrake@gmail.com>
2024-10-14 12:01:05 -0700

    Set up admin for Tasks

--- hours/admin.py
+++ hours/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from hours.models import Task, TaskTranslation
+
+
+class TaskTranslationInline(admin.TabularInline):
+    model = TaskTranslation
+    fk_name = "task"
+
+    def get_max_num(self, request, obj=None, **kwargs):
+        return 2
+
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [
+        TaskTranslationInline,
+    ]
+
+
+admin.site.register(Task, TaskAdmin)

aeab4ad784bc06f4fca83f470bc371f03ed8db6a
Dow Drake <dowdrake@gmail.com>
2025-03-05 17:47:43 -0800

    Remove self-registration option for now.

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,6 @@ from .views import (RegistrationView, ActivateView, CheckEmailView,
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
     path('check-email/', CheckEmailView.as_view(), name="check_email"),
     path('delete-account/', DeleteAccountView.as_view(), name='delete_account'),
